245cef7eb928a5c8a7566e14f1e418fbc8947cdd
Yusun <solarpy@hanyang.ac.kr>
2022-02-18 00:26:15 +0900

    Bootstrap : 사용

---
+++ shop/src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      안뇽
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
+      <button type="button" class="btn btn-success">
+        Success
+      </button>
+    </div>
+  );
+}
+
+export default App;
